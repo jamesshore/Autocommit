029afe178029d839d2eb0e837449ffaa3d09a8c0
James Shore <jshore@jamesshore.com>
2012-05-17 18:38:27 -0700

    Basic Jake wrapper

--- jake.sh
+++ jake.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
 node node_modules/.bin/jake $*
+if [ $? = 0 ]; then
+	echo "Wrapper: Build Succeeded"
+else
+	echo "Wrapper: Build Failed"
+fi
--- test/lint.js
+++ test/lint.js
@@ -3,6 +3,6 @@ require("should");
 
 describe("Test runner", function() {
 	it("should run", function(){
-		(1).should.equal(2);
+		(1).should.equal(1);
 	});
 });

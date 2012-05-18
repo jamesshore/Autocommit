21794b6128069e46ec2036b7c2b299b22682be30
James Shore <jshore@jamesshore.com>
2012-05-18 16:18:49 -0700

    Autocommit: Build state toggled to FAIL

--- test/lint_runner.js
+++ test/lint_runner.js
@@ -6,8 +6,6 @@ var lint = require("../src/lint_runner.js");
 
 describe("Lint Runner", function() {
 	it("should pass good source code", function(){
-		var a = true;
-		a.should.be.ok;
-//		lint.validateSource("var a = 1;").should.be.true;
+		lint.validateSource("var a = 1;").should.be.true;
 	});
 });

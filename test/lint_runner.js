cbd94120904f12a89fe8968d78051339f4d83cde
James Shore <jshore@jamesshore.com>
2012-05-18 18:18:01 -0700

    Autocommit: Build state toggled to FAIL

--- test/lint_runner.js
+++ test/lint_runner.js
@@ -3,7 +3,7 @@
 var expect = require("expect.js");
 var lint = require("../src/lint_runner.js");
 
-describe("Lint Runner", function() {
+describe("Lint runner", function() {
 	it("should pass good source code", function(){
 		expect(lint.validateSource("var a = 1;")).to.be(true);
 	});
@@ -18,5 +18,31 @@ describe("Lint Runner", function() {
 
 	it("should respect globals", function() {
 		expect(lint.validateSource("a = 1;", { undef: true }, { a: true })).to.be(true);
+	});
+});
+
+describe("Error reporting", function() {
+	// stdout inspection code courtesy of http://userinexperience.com/?p=714
+	function setup(callback) {
+		var write = process.stdout.write;
+
+		process.stdout.write = (function(stub) {
+			function(string, encoding, fd) {
+				stub.apply(process.stdout, arguments);
+				callback(string, encoding, fd);
+			};
+		})(process.stdout.write);
+
+		return function() {
+			process.stdout.write = write;
+		};
+	};
+
+	it("should test console.log", function() {
+		var unhook = setup(function(string, encoding, fd) {
+			expect(string).to.equal("foo");
+		});
+		console.log("foo");
+		unhook();
 	});
 });
